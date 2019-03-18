d504efdd662665df6c1887a5a08cb06aff1514fc
cg <18251920942@163.com>
2019-03-18 10:15:31 +0800

    1、更新了链表实现栈，
    2、更新了对数组栈，链表栈的对比。

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,8 @@ public class Main {
 
         return (endTime - startTime) / 1000000000.0;
     }
-    public static void main(String[] args) {
 
-        //数组队列和循环队列的效率对比。
-        int opCount = 100000;
-
+    public static void arrayLoopQueueCompare(int opCount){
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
         double time1 = testQueue(arrayQueue, opCount);
         System.out.println("ArrayQueue, time: " + time1 + " s");
@@ -28,5 +25,62 @@ public class Main {
         LoopQueue<Integer> loopQueue = new LoopQueue<>();
         double time2 = testQueue(loopQueue, opCount);
         System.out.println("loopQueue, time: " + time2 + " s");
+    }
+
+
+    private static double testStack(Stack<Integer> stack, int opCount){
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for(int i = 0; i < opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        for(int i = 0; i < opCount; i++)
+            stack.pop();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    /**
+     * opCount = 100000;
+     * ArrayStack, time: 0.050035297 s
+     * linkedListStack, time: 0.018864557 s
+     *
+     * opCount = 10000000
+     * ArrayStack, time: 2.795067278 s
+     * linkedListStack, time: 3.936748134 s
+     *
+     * opCount的不同，对应的运行时间也是不一样的。具体的规律难以总结，但是原因无非以下几条：
+     *
+     *  1、ArrayStack在数据扩容，缩容的时候，会存在数据搬迁的问题。
+     *
+     *  2、linkedListStack 会存在new节点的问题。
+     *
+     *  3、以上两点都会会受到opCount取值大小的影响。
+     *
+     * @param opCount
+     */
+    public static void arrayLinkedStackCompare(int opCount){
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("linkedListStack, time: " + time2 + " s");
+    }
+
+    public static void main(String[] args) {
+
+        //数组队列和循环队列的效率对比。
+        int opCount = 100000;
+//        arrayLoopQueueCompare(opCount);
+
+        arrayLinkedStackCompare(opCount);
+
+
     }
 }
